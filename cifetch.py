cce65d8cd37c4da1b8820ca22614ca4926e2357e
Abel Navarro <abel.navarro@gmail.com>
2016-04-14 14:17:16 +0200

    uncompressing zip

--- cifetch.py
+++ cifetch.py
@@ -4,7 +4,8 @@ import argparse
 import os
 import urllib2
 import ssl
-import sys
+import zipfile
+
 
 def main():
     parser = argparse.ArgumentParser(description='cifetch - supportconfig fetcher from ci.suse.de')
@@ -23,19 +24,23 @@ def main():
     filepath = directory + "/artifacts.zip"
     if os.path.isfile(filepath):
         print "file was already downloaded"
-        sys.exit(1)
  
-
-    # Download file
-    url = "https://ci.suse.de/job/openstack-mkcloud/{}/artifact/.artifacts/*zip*/.artifacts.zip".format(args.build_number)
-    print "url: " + url
+    else:
+        # Download file
+        url = "https://ci.suse.de/job/openstack-mkcloud/{}/artifact/.artifacts/*zip*/.artifacts.zip".format(args.build_number)
+        print "url: " + url
  
-    context = ssl._create_unverified_context()       
-    fileurl = urllib2.urlopen(url, context=context)
-    with open(filepath, 'wb') as output:
-        output.write(fileurl.read())
+        context = ssl._create_unverified_context()       
+        fileurl = urllib2.urlopen(url, context=context)
+        with open(filepath, 'wb') as output:
+            output.write(fileurl.read())
  
     # Unzip file regardless of previous content
+    zipfile.ZipFile(filepath).extractall(directory)
+    print "Unziped file."
+
+    os.symlink(directory + "/.artifacts", directory + "/artifacts")
+
 
 if __name__ == '__main__':
     main()
