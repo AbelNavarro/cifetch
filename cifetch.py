5d62a895c6df6ae795dfd27e2e0b0bdc635be2c3
Abel Navarro <abel.navarro@gmail.com>
2016-04-14 13:42:41 +0200

    directory creation

--- cifetch.py
+++ cifetch.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python
 
 import argparse
-
+import os
 
 def main():
     parser = argparse.ArgumentParser(description='cifetch - supportconfig fetcher from ci.suse.de')
+    parser.add_argument('build_number', type=int, help="Build number, i.e. 28945")
     args = parser.parse_args()
 
-    print "done."
+    print "build_number: {}".format(args.build_number)
+
+    # If the directory already created for this build?
+    directory="/home/abel/work/ci.suse.de/{}".format(args.build_number)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Is the zip file already downloaded?
+
+    # Unzip file regardless of previous content
 
 if __name__ == '__main__':
     main()
